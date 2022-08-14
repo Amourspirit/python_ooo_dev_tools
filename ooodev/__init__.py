f3f4d70123fb12bf2c895cbf1badc8c3db292b24
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-08-14 11:09:43 -0400

    update config

--- ooodev/__init__.py
+++ ooodev/__init__.py
@@ -1,6 +1,7 @@
-import os
+# coding: utf-8
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
-    version = f.read().strip()
+# importing using a text file as version source is not supported when packaging scripts using stickytape.
+# with open(os.path.join(os.path.dirname(__file__), "VERSION"), "r", encoding="utf-8") as f:
+#     version = f.read().strip()
 
-__version__ = version
+__version__ = "0.4.12"
