79d643ec08ffacf1b1436a55984be0f0f70f546f
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-12-21 13:21:12 -0500

    update cell objects

--- ooodev/utils/data_type/range_values.py
+++ ooodev/utils/data_type/range_values.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING
 from dataclasses import dataclass
-from typing import overload
+from typing import cast, overload
+
+from . import cell_obj as mCellObj
+from . import cell_values as mCellVals
 from . import range_obj as mRngObj
 from .. import table_helper as mTb
 from ...office import calc as mCalc
@@ -8,6 +12,9 @@ from ..decorator import enforce
 
 import uno
 from ooo.dyn.table.cell_range_address import CellRangeAddress
+
+if TYPE_CHECKING:
+    from ooo.lo.table.cell_address import CellAddress
 
 
 @enforce.enforce_types
@@ -20,17 +27,24 @@ class RangeValues:
     """
 
     col_start: int
-    """Column start such as ``0``"""
+    """Column start such as ``0``. Must be non-negative integer"""
     col_end: int
-    """Column end such as ``3``"""
+    """Column end such as ``3``. Must be non-negative integer"""
     row_start: int
-    """Row start such as ``0``"""
+    """Row start such as ``0``. Must be non-negative integer"""
     row_end: int
-    """Row end such as ``125``"""
+    """Row end such as ``125``. Must be non-negative integer"""
     sheet_idx: int = -1
     """Sheet index that this range value belongs to"""
 
     def __post_init__(self):
+        cr_vals = (self.col_start, self.row_start, self.col_end, self.row_end)
+        for val in cr_vals:
+            if val < 0:
+                raise ValueError(
+                    f"All indexes must be greater than 0. Column Range ({self.col_start}:{self.col_end}), Row Range - ({self.row_start}:{self.row_end})"
+                )
+
         if self.sheet_idx < 0:
             try:
                 idx = mCalc.Calc.get_sheet_index()
@@ -172,3 +186,96 @@ class RangeValues:
             bool: ``True`` if single cell; Otherwise, ``False``
         """
         return self.is_single_col() and self.is_single_row()
+
+    # region contains()
+
+    @overload
+    def contains(self, cell_obj: mCellObj.CellObj) -> bool:
+        ...
+
+    @overload
+    def contains(self, cell_addr: CellAddress) -> bool:
+        ...
+
+    @overload
+    def contains(self, cell_vals: mCellVals.CellValues) -> bool:
+        ...
+
+    @overload
+    def contains(self, cell_name: str) -> bool:
+        ...
+
+    def contains(self, *args, **kwargs) -> bool:
+        """
+        Gets if current instance contains a cell value.
+
+        Args:
+            cell_obj (CellObj): Cell object
+            cell_addr (CellAddress): Cell address
+            cell_vals (CellValues): Cell Values
+            cell_name (str): Cell name
+
+        Returns:
+            bool: ``True`` if instance contains cell; Otherwishe, ``False``.
+
+        Note:
+            If cell input contains sheet info the it is use in comparsion.
+            Otherwise sheet is ignored.
+        """
+        ordered_keys = (1,)
+        kargs_len = len(kwargs)
+        count = len(args) + kargs_len
+
+        def get_kwargs() -> dict:
+            ka = {}
+            if kargs_len == 0:
+                return ka
+            valid_keys = ("cell_obj", "cell_vals", "cell_name", "cell_addr")
+            check = all(key in valid_keys for key in kwargs.keys())
+            if not check:
+                raise TypeError("contains() got an unexpected keyword argument")
+            for key in valid_keys:
+                if key in kwargs:
+                    ka[1] = kwargs[key]
+                    break
+            return ka
+
+        if count != 1:
+            raise TypeError("contains() got an invalid number of arguments")
+
+        kargs = get_kwargs()
+        for i, arg in enumerate(args):
+            kargs[ordered_keys[i]] = arg
+
+        arg = kargs[1]
+        if isinstance(arg, mCellObj.CellObj):
+            cvs = arg.get_cell_values()
+            col = cvs.col
+            row = cvs.row
+            idx = cvs.sheet_idx
+        elif isinstance(arg, mCellVals.CellValues):
+            col = arg.col
+            row = arg.row
+            idx = arg.sheet_idx
+        elif isinstance(arg, str):
+            cvs = mCellVals.CellValues.from_cell(arg)
+            col = cvs.col
+            row = cvs.row
+            idx = cvs.sheet_idx
+        else:
+            # CellAddress
+            ca = cast("CellAddress", arg)
+            col = ca.Column
+            row = ca.Row
+            idx = ca.Sheet
+
+        contains = True
+        if idx >= 0:
+            contains = contains and self.sheet_idx == idx
+        contains = contains and self.col_start <= col
+        contains = contains and self.row_start <= row
+        contains = contains and self.col_end >= col
+        contains = contains and self.row_end >= row
+        return contains
+
+    # endregion contains()
--- tests/test_range/test_ranges.py
+++ tests/test_range/test_ranges.py
@@ -311,3 +311,68 @@ def test_cell_obj(loader) -> None:
 
     finally:
         Lo.close_doc(doc)
+
+
+def test_cell_obj_to_cell_values(loader) -> None:
+    from ooodev.utils.data_type.cell_obj import CellObj
+
+    from ooodev.utils.lo import Lo
+    from ooodev.office.calc import Calc
+
+    doc = Calc.create_doc()
+    sheet = Calc.get_sheet(doc)
+
+    try:
+
+        name = "B2"
+        cell1 = CellObj.from_cell(name)
+        cv1 = cell1.get_cell_values()
+        assert cv1.col == 1
+        assert cv1.row == 1
+        assert cv1.sheet_idx == cell1.sheet_idx
+
+        cell2 = CellObj.from_cell(cv1)
+        assert cell2 == cell1
+        cv2 = cell2.get_cell_values()
+        assert cv2 == cv1
+        assert cv2 == name
+
+    finally:
+        Lo.close_doc(doc)
+
+
+@pytest.mark.parametrize(
+    ("rng_name", "cell_name", "expected"),
+    [
+        ("A2:B7", "A2", True),
+        ("A2:B7", "B7", True),
+        ("a2:b7", "A3", True),
+        ("A2:B7", "a5", True),
+        ("A2:B7", "A7", True),
+        ("Sheet1.A2:B7", "Sheet1.A7", True),
+        ("Sheet2.A2:B7", "Sheet2.A7", True),
+        ("Sheet1.A2:B7", "Sheet2.A3", False),
+        ("AA2:AB7", "AA3", True),
+        ("AB2:AB7", "AA3", False),
+        ("A2:B7", "A1", False),
+        ("A2:B7", "A8", False),
+        ("A2:B7", "C100", False),
+        ("A2:B7", "B8", False),
+    ],
+)
+def test_range_obj_contains(rng_name: str, cell_name: str, expected: bool, loader) -> None:
+    from ooodev.utils.lo import Lo
+    from ooodev.office.calc import Calc
+    from ooodev.utils.data_type.cell_obj import CellObj
+    from ooodev.utils.data_type.range_obj import RangeObj
+
+    doc = Calc.create_doc()
+    _ = Calc.insert_sheet(doc, "Sheet2", 1)
+    _ = Calc.get_sheet(doc, 0)
+
+    try:
+        rng = RangeObj.from_range(rng_name)
+        cell = CellObj.from_cell(cell_name)
+        assert rng.contains(cell) == expected
+    finally:
+        Lo.close_doc(doc)
