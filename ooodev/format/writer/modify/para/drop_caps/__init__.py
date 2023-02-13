9ef8f9214ecb71a6349932dd49c135f0d8c71b8a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-02-13 10:25:56 -0500

    Format Write modify drop caps

---
+++ ooodev/format/writer/modify/para/drop_caps/__init__.py
@@ -0,0 +1,5 @@
+import uno
+from ....style.para.kind import StyleParaKind as StyleParaKind
+from ....style.char.kind import StyleCharKind as StyleCharKind
+from .....direct.para.drop_cap.drop_caps import DropCapFmt as DropCapFmt
+from .....modify.para.drop_cap.drop_caps import DropCaps as DropCaps
