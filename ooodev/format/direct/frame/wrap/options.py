c074be2c3d53bd6ed61489db3a2a28ae8d6945a6
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-03-05 04:25:55 -0500

    format writer direct obj wrap

--- ooodev/format/direct/frame/wrap/options.py
+++ ooodev/format/direct/frame/wrap/options.py
@@ -59,9 +59,10 @@ class Options(StyleBase):
         except AttributeError:
             self._supported_services_values = (
                 "com.sun.star.style.Style",
+                "com.sun.star.text.BaseFrame",
+                "com.sun.star.text.TextEmbeddedObject",
                 "com.sun.star.text.TextFrame",
                 "com.sun.star.text.TextGraphicObject",
-                "com.sun.star.text.BaseFrame",
             )
         return self._supported_services_values
 
--- ooodev/format/direct/frame/wrap/settings.py
+++ ooodev/format/direct/frame/wrap/settings.py
@@ -38,9 +38,10 @@ class Settings(StyleBase):
         except AttributeError:
             self._supported_services_values = (
                 "com.sun.star.style.Style",
+                "com.sun.star.text.BaseFrame",
+                "com.sun.star.text.TextEmbeddedObject",
                 "com.sun.star.text.TextFrame",
                 "com.sun.star.text.TextGraphicObject",
-                "com.sun.star.text.BaseFrame",
             )
         return self._supported_services_values
 
