94510c7f075522a63c96d3687bdd6ec89db2787a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-03-05 04:04:06 -0500

    format writer direct obj options

---
+++ tests/test_format/test_direct/test_write/test_obj/test_direct_write_obj_option_protect.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+import pytest
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
+
+import uno
+from ooodev.format.writer.direct.image.options import Names
+from ooodev.format.writer.direct.image.options import Protect
+from ooodev.utils.gui import GUI
+from ooodev.utils.lo import Lo
+from ooodev.office.write import Write
+
+
+def test_write(loader, formula_text) -> None:
+    # delay = 0 if Lo.bridge_connector.headless else 3_000
+    delay = 0
+
+    doc = Write.create_doc()
+    if not Lo.bridge_connector.headless:
+        GUI.set_visible()
+        Lo.delay(500)
+        GUI.zoom(GUI.ZoomEnum.ENTIRE_PAGE)
+    try:
+        cursor = Write.get_cursor(doc)
+
+        style_names = Names(name="skinner", desc="Skinner Pointing", alt="Pointer")
+        style = Protect(size=True, postiion=True, content=True)
+
+        content = Write.add_formula(
+            cursor=cursor,
+            formula=formula_text,
+            styles=(
+                style_names,
+                style,
+            ),
+        )
+
+        f_style = Protect.from_obj(content)
+        assert f_style.prop_size == style.prop_size
+        assert f_style.prop_position == style.prop_position
+        assert f_style.prop_content == style.prop_content
+
+        Lo.delay(delay)
+    finally:
+        Lo.close_doc(doc)
