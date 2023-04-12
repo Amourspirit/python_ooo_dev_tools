b3cec6efac745e56e5c737bf50f2f8e45c5ba7be
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-04-12 12:41:16 -0400

    add font classes specific for Calc

---
+++ ooodev/format/inner/direct/calc/char/font/font_only.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+import uno
+from ooodev.format.inner.direct.write.char.font.font_only import FontOnly as CharFontOnly
+
+
+class FontOnly(CharFontOnly):
+    """
+    Calc Character Font
+
+    Any properties starting with ``prop_`` set or get current instance values.
+
+    All methods starting with ``fmt_`` can be used to chain together font properties.
+
+    .. versionadded:: 0.9.4
+    """
+
+    pass
